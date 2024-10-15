73dfca9d0100489f9e34855f409c33dd3aff92ad
alexchtst-01 <acinatra@gmail.com>
2024-10-15 18:30:19 +0700

    feat: add chatbot module

--- chatbot/ChatBot.py
+++ chatbot/ChatBot.py
@@ -1,24 +1,25 @@
-from dotenv import load_dotenv, dotenv_values
 from groq import Groq
 
-load_dotenv()
-
-GROQ_API = dotenv_values('.env')['GROQ_API']
-
-groq_client = Groq(
-    api_key=GROQ_API
-)
-
-chat_completion = groq_client.chat.completions.create(
-    messages=[{
-        'role': 'user',
-        'content': 'what is the defenition of burn out?',
-    }],
-    model='gemma2-9b-it'
-)
-
-print(chat_completion.choices)
-print(chat_completion.choices[0].message)
-print(chat_completion.choices[0].message.content)
-
-# coba lu bikin class instance biar lebih bisa fleksible nanti kalo dipake
+class ChatBot:
+    def __init__(self, api_key, modelName='gemma2-9b-it', autosavehistory=False):
+        self.__qroqClient__ = Groq(api_key=api_key)
+        self.__model__ = modelName
+        self.__history__ = []
+    
+    def generateText(self, query):
+        answer = self.__qroqClient__.chat.completions.create(
+            messages=[{
+                'role': 'user',
+                'content': query
+            }],
+            model=self.__model__
+        )
+        
+        return answer.choices
+    
+    def getAnswer(self, query):
+        answers = self.generateText(query)
+        return answers[0].message.content
+    
+    def getTopAnswer(self, query):
+        return self.getAnswer(query, 0).message
