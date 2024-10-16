3db85b29deea83dda6a890f4c747c0cde3c2928d
alexchtst-01 <acinatra@gmail.com>
2024-10-16 10:52:14 +0700

    feat: change proper socket server

--- main.py
+++ main.py
@@ -1,14 +1,61 @@
 import socket
 from dotenv import load_dotenv, dotenv_values
+import argparse
 
 load_dotenv()
-SERVER_PORT = int(dotenv_values('.env')['SERVER_PORT'])
+server_port = int(dotenv_values('.env')['SERVER_PORT'])
 
-socket_server = socket.socket()
-print('Successfully connect to the socket connection')
+def server(clientNumber=3, alwaysRun=True):
+    server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    print('socket is ready')
+    
+    server_socket.bind(('', server_port))
+    server_socket.listen(clientNumber)
+    print(f'socket is bind in port : {server_port}, listen to {clientNumber}')
+    
+    if alwaysRun:
+        global Run
+        Run = True
+        try:
+            while Run:
+                client, add = server_socket.accept()
+                print(f'got connection from {add}')
+                
+                client.send('you are connected'.encode())
+                
+        except KeyboardInterrupt:
+            server_socket.close()
+            print('server has been shutdown')
+            Run = False
+    
+    else:
+        client, add = server_socket.accept()
+        print(f'connected with {add}')
+        client.send('you are connected'.encode())
+        
+        client.close()
 
-socket_server.bind(('', SERVER_PORT))
-print(f"server bind to the {SERVER_PORT}")
 
-socket_server.listen(2)
-print(f"server listening to {2} clients")
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--clientnumber",
+    help="client number of server to listened for",
+    type=int,
+    default=3
+)
+
+parser.add_argument(
+    "--isAlwaysRun",
+    help="client number of server to listened for",
+    type=bool,
+    default=True
+)
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    print(args.clientnumber, args.isAlwaysRun)
+    server(
+        clientNumber=args.clientnumber,
+        alwaysRun=args.isAlwaysRun
+    )
